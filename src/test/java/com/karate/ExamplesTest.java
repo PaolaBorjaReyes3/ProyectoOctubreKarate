60ad8cc309fcd53db2e4fcceecc3bd3d1a70e294
Paola <gabrielapaolaborjareyes@gmail.com>
2023-08-21 13:11:42 -0500

    Update config

---
+++ src/test/java/com/karate/ExamplesTest.java
@@ -0,0 +1,13 @@
+package com.karate;
+
+import com.intuit.karate.KarateOptions;
+import com.intuit.karate.junit5.Karate;
+
+
+@KarateOptions(features = "classpath:api", tags = "@Prueba3")
+public class ExamplesTest {
+    @Karate.Test
+    Karate karateTest() {
+        return Karate.run().relativeTo(getClass());
+    }
+}
